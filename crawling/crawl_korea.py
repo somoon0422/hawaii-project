36b8fffd9ceff9a04103c92fabc8d123a540225b
김소희(So-hee-Kim) <116736551+somoon0422@users.noreply.github.com>
2024-07-01 23:43:08 +0000

    test3

--- crawling/crawl_korea.py
+++ crawling/crawl_korea.py
@@ -253,20 +253,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
-
+import chromedriver_autoinstaller
 
 def initialize_webdriver():
-    chromedriver_path = os.path.join(os.path.dirname(__file__), "chromedriver.exe")
-    service = Service(chromedriver_path)
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--remote-debugging-port=9222')
-    
-    driver = webdriver.Chrome(service=service, options=options)
-    return driver
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome()
 
 
 def initialize_logging():
