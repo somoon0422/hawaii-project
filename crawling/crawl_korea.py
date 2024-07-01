1071f6fd25d83b39c0af5fea6b52f9cd9615741c
김소희(So-hee-Kim) <116736551+somoon0422@users.noreply.github.com>
2024-07-01 23:24:18 +0000

    test1

--- crawling/crawl_korea.py
+++ crawling/crawl_korea.py
@@ -256,8 +256,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 def initialize_webdriver():
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    chromedriver_path = os.path.join(os.path.dirname(__file__), "chromedriver.exe")
+    service = Service(chromedriver_path)
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--remote-debugging-port=9222')
+    
+    driver = webdriver.Chrome(service=service, options=options)
     return driver
 
 
